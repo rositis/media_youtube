5dc30e06d0f2a3d0d235510a4cb00eaec28d3e2e
aaronwinborn <aaron@aaronwinborn.com>
2010-01-27 22:51:44 +0000

    by aaron: Begin work on adding 'All/My Youtubes' vertical tabs. by aaron: Add 'YouTube Library' tab to browser w/ all youtube videos.

--- js/media_youtube.fromurl.js
+++ js/media_youtube.fromurl.js
@@ -32,3 +32,91 @@
   //@todo: needs a review.
   Drupal.media.browser.register('media_youtube', Drupal.media.browser.plugin.media_youtube, {});
 })(jQuery);
+
+(function ($) {
+  namespace('media.browser.plugin');
+
+  Drupal.media.browser.plugin.youtube_library = function(mediaBrowser, options) {
+
+    return {
+      mediaFiles: [],
+      init: function() {
+        tabset = mediaBrowser.getTabset();
+        tabset.tabs('add', '#youtube_library', 'YouTube Library');
+        var that = this;
+        mediaBrowser.listen('tabs.show', function (e, id) {
+          if (id == 'youtube_library') {
+            // This is kinda rough, I'm not sure who should delegate what here.
+            mediaBrowser.getActivePanel().addClass('throbber');
+            mediaBrowser.getActivePanel().html('');
+            //mediaBrowser.getActivePanel().addClass('throbber');
+
+            // Assumes we have to refresh everytime.
+            // Remove any existing content
+            mediaBrowser.getActivePanel().append('<ul></ul>');
+            that.browser = $('ul', mediaBrowser.getActivePanel());
+            that.browser.addClass('clearfix');
+            that.getMedia();
+          }
+        });
+      },
+
+      getStreams: function () {
+        return ['youtube://'];
+      },
+
+      getConditions: function () {
+        return {};
+        //return this.settings.conditions;
+      },
+
+      getMedia: function() {
+        var that = this;
+        var callback = mediaBrowser.getCallbackUrl('getMedia');
+        var params = {
+          conditions: JSON.stringify(this.getConditions()),
+          streams: JSON.stringify(this.getStreams())
+        };
+        jQuery.get(
+          callback,
+          params,
+          function(data, status) {
+            that.mediaFiles = data;
+            that.render();
+          },
+          'json'
+        );
+      },
+
+      render: function() {
+        var that = this;
+        mediaBrowser.getActivePanel().removeClass('throbber');
+        if(this.mediaFiles.length < 1) {
+          return;
+        }
+
+        for (var m in this.mediaFiles) {
+          mediaFile = this.mediaFiles[m];
+
+          var listItem = jQuery('<li></li>').appendTo(this.browser)
+            .attr('id', 'media-file-' + mediaFile.fid)
+            .addClass('media-file');
+
+          var imgLink = jQuery('<a href="#"></a>').appendTo(listItem)
+            .html(mediaFile.preview)
+            .bind('click', mediaFile, function(e) {
+              // Notify the main browser
+              //this.selectedMedia = mediaFile;
+              $('div.media-thumbnail img').removeClass('selected');
+              $('div.media-thumbnail img', $(this)).addClass('selected');
+              mediaBrowser.notify('mediaSelected', {mediaFiles: [e.data]});
+              //that.settings.onSelect(mediaFile);
+              return false;
+            });
+        }
+      }
+  };
+};
+
+  Drupal.media.browser.register('youtube_library', Drupal.media.browser.plugin.youtube_library);
+})(jQuery);
