fa9e6c5013c8c45ea746f1a155ade43f95c9ea09
Alex Bronstein <alex@effulgentsia.net>
2010-10-25 22:17:17 +0000

    [#884464] HTML5 video playback support

--- js/media_youtube.js
+++ js/media_youtube.js
@@ -1,184 +1,94 @@
-// $Id$
-
-/**
- *  @file
- *  JavaScript for the Media: YouTube module.
- *
- *  This will send a YouTube link or embed code back to the module,
- *  which will return an embedded clip and fill in the uri on the main form.
- */
 (function ($) {
 
-/**
- *  Send the link back to the module, and fill in elements w/ correct info.
- */
-Drupal.behaviors.mediaYoutubeLink = {
+Drupal.media_youtube = {};
+Drupal.behaviors.media_youtube = {
   attach: function (context, settings) {
-    var ytdb = [];
-    $('.form-item-media-youtube-url input', context).once('mediaYoutubeLink', function () {
-      var uri = this.value;
-      if (!ytdb[uri]) {
-        ytdb[uri] = new Drupal.YTDB(uri);
+    // Check the browser to see if it supports html5 video.
+    var video = document.createElement('video');
+    var html5 = video.canPlayType ? true : false;
+
+    // If it has video, does it support the correct codecs?
+    if (html5) {
+      html5 = false;
+      if (video.canPlayType( 'video/webm; codecs="vp8, vorbis"' ) || video.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"')) {
+        html5 = true;
       }
-      var input = $(this);
-      new Drupal.mediaYoutubeBind(input, ytdb[uri]);
-    });
+    }
+
+    // Put a prompt in the video wrappers to let users know they need flash
+    if (!FlashDetect.installed && !html5){
+      $('.media-youtube-preview-wrapper').each(Drupal.media_youtube.needFlash);
+    }
   }
 };
 
-/**
- * An AutoComplete object.
- */
-Drupal.mediaYoutubeBind = function (input, db) {
-  var yt = this;
-  this.input = input;
-  this.db = db;
-  this.parsing = false;
-  $(this.input)
-    .keyup(function () { yt.parseurl(this); })
-    .blur(function () { yt.parseurl(this); });
-
+Drupal.media_youtube.needFlash = function () {
+  var id = $(this).attr('id');
+  var wrapper = $('.media-youtube-preview-wrapper');
+  var hw = Drupal.settings.media_youtube[id].height / Drupal.settings.media_youtube[id].width;
+  wrapper.html('<div class="js-fallback">' + Drupal.t('You need Flash to watch this video. <a href="@flash">Get Flash</a>', {'@flash':'http://get.adobe.com/flashplayer'}) + '</div>');
+  wrapper.height(wrapper.width() * hw);
 };
 
-Drupal.mediaYoutubeBind.prototype.parseurl = function (input) {
-  input.delay = 500;
-  // Initiate delayed parse.
-  if (input.timer) {
-    clearTimeout(input.timer);
-  }
-  if (input.value.length == 0) {
-    return;
-  }
-  if (input.value == input.current_value) {
-    return;
-  }
-  input.current_value = input.value;
-  input.url = Drupal.settings.mediaYoutube.parse_url + '?parse=' + Drupal.encodePath(input.value);
-  input.timer = setTimeout(function () {
+Drupal.media_youtube.insertEmbed = function (embed_id) {
+  var videoWrapper = $('#' + embed_id + '.media-youtube-preview-wrapper');
+  var settings = Drupal.settings.media_youtube[embed_id];
 
-    // Ajax GET request for autocompletion.
-    $.ajax({
-      type: 'GET',
-      url: input.url,
-      dataType: 'json',
-      success: function (val) {
-        if (val.status == 'ok') {
-          $('input.media-file-uri').val(val.uri);
-          $('.media-youtube-preview-markup').html(val.preview);
-        }
-        else {
-          $('input.media-file-uri').val('');
-        }
-      },
-      error: function (xmlhttp) {
-        alert(Drupal.ajaxError(xmlhttp, input.url));
-      }
-    });
-  }, input.delay);
-}
+  // Calculate the ratio of the dimensions of the embed.
+  settings.hw = settings.height / settings.width;
 
-/**
- * Handler for the "keyup" event.
- */
-Drupal.mediaYoutubeBind.prototype.onkeyup = function (input, e) {
-  if (!e) {
-    e = window.event;
+  // Replace the object embed with YouTube's iframe. This isn't done by the
+  // theme function because YouTube doesn't have a no-JS or no-Flash fallback.
+  var video = $('<iframe class="youtube-player" type="text/html" frameborder="0"></iframe>');
+  var src = 'http://www.youtube.com/embed/' + settings.video_id;
+
+  // Allow other modules to modify the video settings.
+  settings.options = {wmode : 'opaque'};
+  $(window).trigger('media_youtube_load', settings);
+
+  // Merge YouTube options (such as autoplay) into the source URL.
+  var query = $.param(settings.options);
+  if (query) {
+    src += '?' + query;
   }
-  switch (e.keyCode) {
-    case 16: // shift.
-    case 17: // ctrl.
-    case 18: // alt.
-    case 20: // caps lock.
-    case 33: // page up.
-    case 34: // page down.
-    case 35: // end.
-    case 36: // home.
-    case 37: // left arrow.
-    case 38: // up arrow.
-    case 39: // right arrow.
-    case 40: // down arrow.
-      return true;
 
-    case 9:  // tab.
-    case 13: // enter.
-    case 27: // esc.
-      this.hidePopup(e.keyCode);
-      return true;
+  // Set up the iframe with its contents and add it to the page.
+  video
+    .attr('id', settings.id)
+    .attr('width', settings.height)
+    .attr('height', settings.width)
+    .attr('src', src);
+  videoWrapper.html(video);
 
-    default: // All other keys.
-      if (input.value.length > 0)
-        this.populatePopup();
-      else
-        this.hidePopup(e.keyCode);
-      return true;
+  // Bind a resize event to handle fluid layouts.
+  $(window).bind('resize', Drupal.media_youtube.resizeEmbeds);
+
+  // For some reason Chrome does not properly size the container around the
+  // embed and it will just render the embed at full size unless we set this
+  // timeout.
+  if (!$('.lightbox-stack').length) {
+    setTimeout(Drupal.media_youtube.resizeEmbeds, 1);
   }
 };
 
-
-/**
- * An AutoComplete DataBase object.
- */
-Drupal.YTDB = function (uri) {
-  this.uri = uri;
-  this.delay = 300;
-  this.cache = {};
+Drupal.media_youtube.resizeEmbeds = function () {
+  $('.media-youtube-preview-wrapper').each(Drupal.media_youtube.resizeEmbed);
 };
 
-/**
- * Performs a cached and delayed search.
- */
-Drupal.YTDB.prototype.search = function (searchString) {
-  var db = this;
-  this.searchString = searchString;
+Drupal.media_youtube.resizeEmbed = function () {
+  var context = $(this).parent();
+  var video = $(this).children(':first-child');
+  var hw = Drupal.settings.media_youtube[$(this).attr('id')].hw;
+  // Change the height of the wrapper that was given a fixed height by the
+  // YouTube theming function.
+  $(this)
+    .height(context.width() * hw)
+    .width(context.width());
 
-  // See if this string needs to be searched for anyway.
-  searchString = searchString.replace(/^\s+|\s+$/, '');
-  if (searchString.length <= 0 ||
-    searchString.charAt(searchString.length - 1) == ',') {
-    return;
-  }
-
-  // See if this key has been searched for before.
-  if (this.cache[searchString]) {
-    return this.owner.found(this.cache[searchString]);
-  }
-
-  // Initiate delayed search.
-  if (this.timer) {
-    clearTimeout(this.timer);
-  }
-  this.timer = setTimeout(function () {
-    db.owner.setStatus('begin');
-
-    // Ajax GET request for autocompletion.
-    $.ajax({
-      type: 'GET',
-      url: Drupal.settings.mediaYoutube.parse_url + '/' + Drupal.encodePath(searchString),
-      dataType: 'json',
-      success: function (matches) {
-        if (typeof matches.status == 'undefined' || matches.status != 0) {
-          db.cache[searchString] = matches;
-          // Verify if these are still the matches the user wants to see.
-          if (db.searchString == searchString) {
-            db.owner.found(matches);
-          }
-          db.owner.setStatus('found');
-        }
-      },
-      error: function (xmlhttp) {
-        alert(Drupal.ajaxError(xmlhttp, db.uri));
-      }
-    });
-  }, this.delay);
-};
-
-/**
- * Cancels the current autocomplete request.
- */
-Drupal.YTDB.prototype.cancel = function () {
-  if (this.owner) this.owner.setStatus('cancel');
-  if (this.timer) clearTimeout(this.timer);
-  this.searchString = '';
+  // Change the attributes on the embed to match the new size.
+  video
+    .height(context.width() * hw)
+    .width(context.width());
 };
 
 })(jQuery);
