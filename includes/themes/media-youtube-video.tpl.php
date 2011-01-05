5c24ab25b787abba95e8171a57bb46078b8ee31a
aaronwinborn <aaron@aaronwinborn.com>
2011-01-05 21:06:57 +0000

    by aaron: Add support for video to styles 2.

---
+++ includes/themes/media-youtube-video.tpl.php
@@ -0,0 +1,22 @@
+<?php
+// $Id$
+
+/**
+ * @file media_youtube/includes/themes/media-youtube-video.tpl.php
+ *
+ * Template file for theme('media_youtube_video').
+ *
+ * Variables available:
+ *  $uri - The uri to the YouTube video, such as youtube://v/xsy7x8c9.
+ *  $video_id - The unique identifier of the YouTube video.
+ *  $width - The width to render.
+ *  $height - The height to render.
+ *  $autoplay - If TRUE, then start the player automatically when displaying.
+ *  $fullscreen - Whether to allow fullscreen playback.
+ */
+?>
+<div class="media-youtube-outer-wrapper" id="media-youtube-<?php print $id; ?>">
+  <div class="media-youtube-preview-wrapper" id="<?php print $wrapper_id; ?>">
+    <?php print $output; ?>
+  </div>
+</div>
